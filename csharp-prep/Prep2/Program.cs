86f8fa0b627ce827e8a2f1daf798f82e644729e2
Francisco Sancen <fr.sancen@gmail.com>
2023-09-16 08:50:51 -0600

    Add score program to prep2.cs

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,72 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        
+        Console.Write("What is score?: ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+
+        string letter = "";
+
+        string signScore = "";
+
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "D";
+        }
+
+        else 
+        {
+            letter = "F";
+        }
+
+        int secondDigit = grade % 10;
+
+        if (secondDigit >= 7)
+        {
+            signScore = "+";
+        }
+
+        else if (secondDigit > 3)
+        {
+            signScore = "";
+        }
+
+        else if (secondDigit <= 3)
+        {
+            signScore = "-";
+        }
+
+        else if (letter == "F")
+        {
+            signScore = "";
+        }
+
+        if (grade >= 70)
+        {
+            Console.WriteLine($"Congratulatons you pass the class with '{signScore}{letter}'!");
+        }
+
+        else
+        {
+            Console.WriteLine($"Your grade is '{letter}' keep trying, you can do it the next time.");
+        }
     }
-}
+}   
